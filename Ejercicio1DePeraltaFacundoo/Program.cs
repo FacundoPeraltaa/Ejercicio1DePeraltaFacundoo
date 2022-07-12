d2c17b40f0deb631c35e5b2280ac8f9aa1f5f88f
facup <facup@NQ3X>
2022-07-12 19:15:42 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1DePeraltaFacundoo/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1DePeraltaFacundoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolarparalelo, dolaroficial;
+            double dolares, pesos, peso, euro, euros;
+
+            Console.WriteLine(" ingrese cantidad de dolares");
+            dolares = double.Parse(Console.ReadLine());
+
+            dolaroficial = 230;
+            dolarparalelo = 280;
+
+            pesos = (dolares * dolaroficial);
+            peso = (dolares * dolarparalelo);
+
+            Console.Write($" Con la cantidad de {dolares} dolares, compras {pesos} pesos en el Mercado Oficial");
+             Console.Write($" Con la cantidad de {dolares} dolares, compras {peso} pesos en el Mercado Paralelo");
+
+            euro = 1.17;
+            euros = dolares * euro;
+
+            Console.Write($" La cantidad de {dolares} dolares, equivalen a {euros} euros");
+
+            Console.ReadLine();
+        }
+
+    }
+}
